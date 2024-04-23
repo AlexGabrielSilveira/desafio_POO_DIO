10371db1db503ef54ef9262264458a6efb7beada
Alex <61164399+AlexGabrielSilveira@users.noreply.github.com>
2024-04-23 11:16:04 -0300

    Add files via upload

---
+++ Models/Nokia.cs
@@ -0,0 +1,13 @@
+namespace DesafioPOO.Models
+{
+    public class Nokia : Smartphone
+    {
+        public Nokia(string numero, string modelo, string imei, string memoria) : base(numero, modelo,imei,memoria) {
+            
+        }
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"instalando o aplicativo: {nomeApp} no Nokia");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using DesafioPOO.Models;
+
+Console.WriteLine("Nokia tijolão na voz: ");
+Smartphone nokia = new Nokia(numero: "1194221-0997", modelo: "modelo 001",imei: "1111111111111", memoria: "4GB");
+
+nokia.Ligar();
+nokia.InstalarAplicativo("tinder");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Celular de burgues: ");
+Smartphone iphone = new Iphone(numero: "194221-0998", modelo: "modelo 2", imei: "222222222222", memoria: "264GB");
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("ZAPZAP");
+
